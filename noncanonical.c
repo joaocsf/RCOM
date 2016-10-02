526ee2311383ac733e55b96b294e734debab006f
joaocsf <noobcsf@hotmail.com>
2016-10-02 14:31:37 +0100

    Added Files from the first class

---
+++ noncanonical.c
@@ -0,0 +1,102 @@
+/*Non-Canonical Input Processing*/
+ 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+ 
+volatile int STOP=FALSE;
+ 
+int main(int argc, char** argv)
+{
+ 
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+    int i, sum = 0, speed = 0;
+   
+    if ( (argc < 2) ||
+         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
+ 
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+ 
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+ 
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0; //OPOST;
+ 
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+ 
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+ 
+ 
+    tcflush(fd, TCIFLUSH);
+ 
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+ 
+    /*testing*/
+    //buf[25] = '\n';
+ 
+    gets(buf);     
+ 
+    printf("Sending Data : ");
+ 
+    int tamanho = strlen(buf) + 1;
+    int escrito = 0;
+    int offset = 0;
+    while( (escrito = write(fd,buf + offset,tamanho)) < tamanho ){
+        offset += escrito;
+        tamanho -= escrito;
+        printf(".");
+    }
+    printf("\n%d\n",escrito);
+    printf("\n all bytes written \n");
+ 
+    //Leitura
+ 
+    char content;
+    while(1){
+        read(fd, &content, 1);
+        if(content == '\0')
+            break;
+        else
+            printf("%c",content);
+               
+    }
+   
+    printf("\n Done!\n");  
+ 
+ 
+    sleep(2);
+ 
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+    close(fd);
+    return 0;
+}
