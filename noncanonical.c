94a5924a0f25b761fd110633ed7bde94c43e9e03
Jose Martins <lluismmartins7@gmail.com>
2016-10-03 12:28:41 +0100

    guiao 2 completo

--- noncanonical.c
+++ noncanonical.c
@@ -1,5 +1,5 @@
 /*Non-Canonical Input Processing*/
- 
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,92 +7,186 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
+#include<signal.h>
+#include <unistd.h>
+
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
- 
-volatile int STOP=FALSE;
- 
+
+#define F 0x7E
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC_OK 4
+#define STOP 5
+
+char buffer[5] = {F,A, C_SET, A ^ C_SET, F};
+int fileID;
+void sendBytes(int fd, char* buf, int tamanho){
+	printf("SendBytes Initialized\n");
+	int escrito = 0;
+    int offset = 0;
+    while( (escrito = write(fd,buf + offset,tamanho)) < tamanho ){
+        offset += escrito;
+        tamanho -= escrito;
+    }
+}
+
+char parseSupervision(int fd){
+	char buf[5];
+	char status = START;
+	while(status != STOP){
+		char readed;
+		read(fd, &readed, 1);
+
+		switch(status){
+			case START:
+				if(readed == F){
+					status = FLAG_RCV;
+					buf[0] = readed;
+				}
+				break;
+			case FLAG_RCV:
+				switch(readed){
+					case F:
+						break;
+					case A:
+						status = A_RCV;
+						buf[1] = readed;
+						break;
+					default:
+						status = START;
+						break;
+				}
+				break;
+			case A_RCV:
+				switch(readed){
+					case F:
+						status = FLAG_RCV;
+						break;
+					case C_SET:
+					case C_UA:
+						status = C_RCV;
+						buf[2] = readed;
+						break;
+					default:
+						status = START;
+						break;
+				}
+				break;
+			case C_RCV:
+				switch(readed){
+					case F:
+						status = FLAG_RCV;
+						break;
+					default:
+						if(buf[1] ^ buf[2] == readed){
+							buf[3] = readed;
+							status = BCC_OK;
+						}else
+							status = START;
+						break;
+				}
+				break;
+			case BCC_OK:
+				switch(readed){
+					case F:
+						buf[4] = readed;
+						status = STOP;
+						break;
+					default:
+						status = START;
+						break;
+				}
+				break;
+		}
+	}
+	alarm(0);
+	return buf[2];
+}
+
+void timeOut(){
+	sendBytes(fileID, buffer, 5);
+	alarm(3);
+}
+
 int main(int argc, char** argv)
 {
- 
+
+	signal(SIGALRM, timeOut);
+
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[255];
+
     int i, sum = 0, speed = 0;
-   
+
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
- 
+
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
- 
+
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
- 
+
     bzero(&newtio, sizeof(newtio));
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0; //OPOST;
- 
+
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
- 
+
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
- 
- 
+
+
     tcflush(fd, TCIFLUSH);
- 
+
     if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
- 
+
     /*testing*/
     //buf[25] = '\n';
- 
-    gets(buf);     
- 
+
+
+
     printf("Sending Data : ");
- 
-    int tamanho = strlen(buf) + 1;
-    int escrito = 0;
-    int offset = 0;
-    while( (escrito = write(fd,buf + offset,tamanho)) < tamanho ){
-        offset += escrito;
-        tamanho -= escrito;
-        printf(".");
-    }
-    printf("\n%d\n",escrito);
+
+	fileID = fd;
+	sendBytes(fd, buffer, 5);
+	alarm(3);
+
     printf("\n all bytes written \n");
- 
+
     //Leitura
- 
-    char content;
-    while(1){
-        read(fd, &content, 1);
-        if(content == '\0')
-            break;
-        else
-            printf("%c",content);
-               
-    }
-   
-    printf("\n Done!\n");  
- 
- 
+
+    char content = parseSupervision(fd);
+
+	printf("0x%x" , content);
+
+
+    printf("\n Done!\n");
+
+
     sleep(2);
- 
+
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
--- noncanonicalInput.c
+++ noncanonicalInput.c
@@ -1,124 +1,196 @@
 /*Non-Canonical Input Processing*/
- 
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
- 
+#include <string.h>
+#include <stdlib.h>
+#include<signal.h>
+#include <unistd.h>
+
 #define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
- 
-volatile int STOP=FALSE;
- 
+
+#define F 0x7E
+#define A 0x03
+#define C_SET 0x03
+#define C_UA 0x07
+
+#define START 0
+#define FLAG_RCV 1
+#define A_RCV 2
+#define C_RCV 3
+#define BCC_OK 4
+#define STOP 5
+
+char buffer[5] = {F,A, C_UA, A ^ C_UA, F};
+
+int fileID;
+
+void sendBytes(int fd, char* buf, int tamanho){
+    printf("SendBytes Initialized\n");
+    int escrito = 0;
+    int offset = 0;
+    while( (escrito = write(fd,buf + offset,tamanho)) < tamanho ){
+        offset += escrito;
+        tamanho -= escrito;
+    }
+}
+
+char parseSupervision(int fd){
+	char buf[5];
+    char status = START;
+    while(status != STOP){
+        char readed;
+        read(fd, &readed, 1);
+
+        switch(status){
+            case START:
+                if(readed == F){
+                    status = FLAG_RCV;
+                    buf[0] = readed;
+                }
+                break;
+            case FLAG_RCV:
+                switch(readed){
+                    case F:
+                        break;
+                    case A:
+                        status = A_RCV;
+                        buf[1] = readed;
+                        break;
+                    default:
+                        status = START;
+                        break;
+                }
+                break;
+            case A_RCV:
+                switch(readed){
+                    case F:
+                        status = FLAG_RCV;
+                        break;
+                    case C_SET:
+                    case C_UA:
+                        status = C_RCV;
+                        buf[2] = readed;
+                        break;
+                    default:
+                        status = START;
+                        break;
+                }
+                break;
+            case C_RCV:
+                switch(readed){
+                    case F:
+                        status = FLAG_RCV;
+                        break;
+                    default:
+                        if(buf[1] ^ buf[2] == readed){
+                            buf[3] = readed;
+                            status = BCC_OK;
+                        }else
+                            status = START;
+                        break;
+                }
+                break;
+            case BCC_OK:
+                switch(readed){
+                    case F:
+						buf[4] = readed;
+                        status = STOP;
+                        break;
+                    default:
+                        status = START;
+                        break;
+                }
+                break;
+        }
+    }
+    alarm(0);
+    return buf[4];
+}
+
+void timeOut(){
+    sendBytes(fileID, buffer, 5);
+    alarm(3);
+}
+
 int main(int argc, char** argv)
 {
+
+	//signal(SIGALRM, timeOut);
+
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[255];
- 
+
     if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
- 
- 
+
+
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
- 
-   
+
+
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd <0) {perror(argv[1]); exit(-1); }
- 
+
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
       perror("tcgetattr");
       exit(-1);
     }
- 
+
     bzero(&newtio, sizeof(newtio));
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
- 
+
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
- 
+
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
- 
- 
- 
+
+
+
   /*
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) próximo(s) caracter(es)
+    leitura do(s) prÃ³ximo(s) caracter(es)
   */
- 
- 
- 
+
+
+
     tcflush(fd, TCIOFLUSH);
- 
+
     if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
- 
+
     printf("New termios structure set\n");
- 
+
     //receive
- 
+
     int i = 0;
- 
-    /*NEW
- 
-    while (STOP == FALSE){
-        res = read(fd, buf[i], 1);
-        if(buf[i] == '\0')
-            STOP = TRUE;
-        i++;
-    }
- 
-    i = 0;
- 
-    /*\NEW*/
-   
-    while (STOP == FALSE) { //Printing
-    res = read(fd, &buf[i], 1);
-    if (buf[i] == '\0') {
-        STOP = TRUE;
-        printf("\n");
-    } else {
-        //sleep(1);
-        printf("%c", buf[i]);
-        i++;
-    }
-    }
-    //sleep(5);
- 
-    //return
-    res = write(fd, buf, strlen(buf) + 1);
- 
-    //while (STOP==FALSE) {       /* loop for input */
-      //res = read(fd,buf,255);   /* returns after 5 chars have been input */
-      //buf[res]=0;               /* so we can printf... */
-      //printf(":%s:%d\n", buf, res);
-      //if (buf[0]=='z') STOP=TRUE;
-    //}
- 
- 
- 
-  /*
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
-  */
- 
-   
+
+	//Leitura
+	printf("%X\n",parseSupervision(fd));
+
+	//Resending
+	sendBytes(fd,buffer,5);
+
     sleep(2);
- 
+
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
     return 0;
