2b045f67482950ee810567ad3e602dd5cbf5c5d9
joaocsf <noobcsf@hotmail.com>
2016-10-17 12:40:53 +0100

    Small Update

--- noncanonical.c
+++ noncanonical.c
@@ -15,6 +15,7 @@
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
+#define BIT(n) (0x1 << n)
 
 #define F 0x7E
 #define A 0x03
@@ -22,6 +23,7 @@
 #define A_RECIVER 0x01
 #define C_SET 0x03
 #define C_UA 0x07
+#define C_INFO BIT(6)
 
 #define START 0
 #define FLAG_RCV 1
@@ -31,6 +33,12 @@
 #define STOP 5
 #define TRANSMITTER O_WRONLY 
 #define RECEIVER O_RDONLY
+#define ESCAPE 0x7d
+#define ESCAPE_XOR 0x20
+#define RR(n) ((n << 7) | 0x05)
+#define REJ(n) ((n << 7) | 0x01)
+
+#define PACKAGE_LENGTH 5
 
 #define SupervisionSize 5
 
@@ -41,17 +49,67 @@ struct buffer{
 
 int fileID;
 
+
 struct termios oldtio;
 
 
 char * getSupervisionBuf(char address, char control){
-	char* buff = (char*)malloc(sizeof(char) * 5);
+	char* buff = (char*)malloc(sizeof(char) * SupervisionSize);
 	buff[0] = F;
 	buff[1] = address;
 	buff[2] = control;
 	buff[3] = buff[1] ^ buff[2];
 	buff[4] = F;
 	return buff;
+}
+
+char* byteStuffing(char * buf, int length, int stuffingLength){
+	unsigned int lengthR = length + stuffingLength;	
+	char * res = (char*)malloc(sizeof(char)* (lengthR));
+	int offset = 0;
+	int i = 0;
+	for(i; i < length; i++){
+		if(i == 0 || i == length-1){
+			res[i+offset] = buf[i];
+		}else if(buf[i] == F || buf[i] == ESCAPE){
+			res[i+offset] = ESCAPE;
+			offset++;
+			res[i+offset] = buf[i] ^ ESCAPE_XOR; 
+		}else
+			res[i+offset] = buf[i];
+	}	
+	return res;
+}
+
+char * getDataBuf(char addr, char ns, char * data,unsigned int length){
+	
+	int dataLength = (length < PACKAGE_LENGTH)? length : PACKAGE_LENGTH;
+	printf("dataLength %d\n", dataLength);
+	int totalLength = 5 + dataLength + 1;	
+	int buffSize = sizeof(char) * totalLength;
+	char* buff = (char*)malloc(buffSize);
+	
+	buff[0] = F;
+	buff[1] = addr;
+	buff[2] = C_INFO;
+	buff[3] = buff[1] ^ buff[2];
+	
+	unsigned char bcc2 = 0;
+	int stuffSize = 0;
+	int i = 0;
+	for(i; i < dataLength; i++){
+		buff[ 4 + i ] = data[i];
+		bcc2 |= data[i];
+		if(data[i] == F || data[i] == ESCAPE)
+			stuffSize++;
+	}
+	printf("dataLength %x , %d\n", buff[0], totalLength);
+
+	buff[5 + dataLength] = F;
+
+	char * res = byteStuffing(buff, totalLength, stuffSize);
+	free(buff);
+	return res;
 }
 
 void sendBytes(int fd, char* buf, int tamanho){
@@ -167,29 +225,18 @@ int initTransmitter(){
 	return (content == C_UA);	
 }
 
-int initTransmitter(){
-	char * bufToSend = getSupervisionBuf(A,C_SET);
-	
-	setBuffer(&lastBuffer, bufToSend , SupervisionSize);
-	sendLastBuffer(fileID,&lastBuffer);
-	alarm(3);	
-	
-    char content = parseSupervision(fileID);
-	
-	return (content == C_UA);	
-}
-
 int llopen(int porta, unsigned char side){	
-	
+
 	if(porta < 0 || porta > 3){
 		printf("Error: port range is [0;3] given: %d \n", porta);
 		return -1;	
 	}	
     int fd,c, res;
     struct termios newtio;	
+	
 
     int i, sum = 0, speed = 0;
-	char nomePorta[10];
+	char nomePorta[11];
 	strcpy(nomePorta, "/dev/ttyS0");
 	nomePorta[9] = '0' + porta;	
 	
@@ -250,11 +297,27 @@ int llclose(int fd){
 	return res;
 }
 
+int llwrite(int fd, char* buffer, int length){
+	
+}
+
 int main(int argc, char** argv)
 {
 
-	signal(SIGALRM, timeOut);
+	if(argc != 2){
+		printf("You need to specify the port number\n");
+		return -1;
+	}	
 
+	char* res  = getDataBuf(A_SENDER, 0, bytes, 1);
+	printf("0x");
+	for(int i = 0; i < sizeof(res); i++){
+		printf("%x",res[i]);
+	}
+	printf("\n");	
+
+	signal(SIGALRM, timeOut);
+	
 	fileID = llopen(atoi(argv[1]), TRANSMITTER);	
 	
 	printf("Sending Data : ");
