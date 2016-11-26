f722e06e9d19928e3dab46a931a55de66ea94d8a
OEM Configuration (temporary user) <noobcsf@hotmail.com>
2016-11-26 16:19:25 +0000

    Finished tcp client

--- T2/ftp-client.c
+++ T2/ftp-client.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
@@ -50,7 +52,8 @@ void parseUserData(char * link,unsigned int size, struct linkInfo * usrInfo){
 
   if(n1 >= n2){
     usrInfo->usr[0] = '\0';
-    usrInfo->pw[0] = '\0';
+    usrInfo->pw[0] = '-';
+    usrInfo->pw[1] = '\0';
     return;
   }
 
@@ -261,16 +264,43 @@ int ftp_send_cmd(int fd, char * buf, unsigned int size){
   }
   return sent;
 }
+
+void printData(char * buf, unsigned int size){
+  printf("Data:");
+  int i = 0;
+  for(; i < size; i++){
+    printf(" %x ", (char)buf[i]);
+  }
+  printf("\n");
+}
+
 /*Method to receive the status of the last ftp call*/
 int ftp_receive_status(int fd){
 
-  int status;
+  char status[19048];
+  unsigned int received = 0;
+  printf("Status:");
+  while( (received = recv(fd, status , 19048,0)) ){
 
-  if(recv(fd, &status, sizeof(int),0) < 1){
-    perror("Recv");
-    return -1;
+    if(received < 0){
+      perror("Recv");
+      return -1;
+    }
+    printf("%s", status);
+    char* line = strtok((char *)status, "\n");
+    char end = 0;
+    while(line != NULL){
+      if(line[0] >= '1' && line[0] <= '5' && line[3] ==' ')
+        end = 1;
+      line = strtok(NULL, "\n");
+    }
+
+    if(end)
+      break;
   }
-  return status;
+  printf("\n");
+
+  return 1;
 }
 
 unsigned int ftp_cmd(char* buf, char* cmd, char* append){
@@ -280,15 +310,21 @@ unsigned int ftp_cmd(char* buf, char* cmd, char* append){
   memcpy(buf,cmd, cmdLen);
   memcpy(buf + cmdLen ,append, appendLen + 1);
 
-  return cmdLen + appendLen + 1;
+  unsigned int size = cmdLen + appendLen + 1;
+  buf[size] = '\r';
+  buf[size + 1] = '\n';
+  buf[size + 2] = '\0';
+
+  return cmdLen + appendLen + 3;
 }
 
 void ftp_login(int fd, char* usr, char* pw){
   char buff[4028];
-
+  printf("Login\n");
   unsigned int size = 0;
 
   size = ftp_cmd(buff, "USER ", usr);
+  printf("%s\n", buff);
   if(ftp_send_cmd(fd, buff, size) < 0)
     ftp_error("Error Sending USER Command!");
 
@@ -304,6 +340,98 @@ void ftp_login(int fd, char* usr, char* pw){
 
 }
 
+void ftp_changePath(int fd, char* path){
+  char buff[4028];
+  printf("Change Path\n");
+  unsigned int size = 0;
+
+  size = ftp_cmd(buff, "CWD ", path);
+  if(ftp_send_cmd(fd, buff, size) < 0)
+    ftp_error("Error Sending CWD Command!");
+
+  if(ftp_receive_status(fd) < 1)
+    ftp_error("Error Receving CWD confirmation!");
+
+}
+
+unsigned short ftp_parsePASV(char * ip, char* msg){
+  int i1,i2,i3,i4,p1,p2;
+
+  sscanf(msg, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &i1,&i2,&i3,&i4,&p1,&p2);
+
+  sprintf(ip, "%d.%d.%d.%d", i1,i2,i3,i4);
+  return p1 * 256 + p2;
+}
+
+int ftp_setPASV(int fd){
+  char buff[1024];
+  unsigned int size;
+  size = ftp_cmd(buff, "PASV", "");
+  if(ftp_send_cmd(fd, buff, size) < 0)
+    ftp_error("Error Sending PASV Command!");
+
+  char passvMSG[1024];
+  if(recv(fd, passvMSG, 1024, 0) < 1)
+    ftp_error("Error Receving PASV confirmation!");
+  char address[1024];
+  unsigned short port = ftp_parsePASV(address, passvMSG);
+
+  return initConnection(address, port);
+}
+
+void ftp_getFile(int fd, char * file){
+  char buff[20028];
+  unsigned int fd2 = ftp_setPASV(fd);
+
+  printf("Get File\n");
+  unsigned int size = 0;
+
+  size = ftp_cmd(buff, "RETR ", file);
+  if(ftp_send_cmd(fd, buff, size) < 0)
+    ftp_error("Error Sending RETR Command!");
+
+  if(ftp_receive_status(fd) < 1)
+    ftp_error("Error Receving RETR confirmation!");
+
+
+
+  int dataReceived = 0;
+  int vezes = 0;
+  int fileFD = open( file, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+  if(fileFD < 0 ){ printf("Error Creating file! %s\n", file); exit(-1);}
+
+  while( (dataReceived = read(fd2,buff, 20028))){
+    if(dataReceived < 0){
+      printf("Error! Downloading the file!");
+      exit(-1);
+    }
+    int dataWritten = 0;
+    printf("Data: %d - %d\n", dataReceived, vezes++);
+
+    while( dataWritten < (dataReceived) ){
+      dataWritten += write(fileFD, buff + dataWritten, dataReceived - dataWritten);
+    }
+  }
+  close(fileFD);
+  close(fd2);
+}
+
+void ftp_disconnect(int fd){
+  char buff[20];
+  unsigned int size;
+  if(ftp_receive_status(fd) < 1)
+    ftp_error("Error Receving Download confirmation!");
+
+  size = ftp_cmd(buff, "QUIT ", "");
+  if(ftp_send_cmd(fd, buff, size) < 0)
+    ftp_error("Error Sending QUIT Command!");
+
+  if(ftp_receive_status(fd) < 1)
+    ftp_error("Error Receving QUIT confirmation!");
+
+  close(fd);
+}
+
 int main(int argc, char** argv){
 
 	int	sockfd;
@@ -311,29 +439,42 @@ int main(int argc, char** argv){
 	char	buf[2];
   unsigned short port = 21;
 	int	bytes;
+  /*
   test_ipParser("ftp://user:password@fe.up.pt:34/path/file.txt");
   test_ipParser("ftp://user:password@fe.up.pt/path/file.txt");
   test_ipParser("ftp://fe.up.pt/path/file2.txt");
   test_ipParser("ftp://fe.up.pt:870/path/file2.txt");
-  test_ipParser("ftp://fe.up.pt:870/file2.txt");
+  test_ipParser("ftp://fe.up.pt:870/file2.txt"); */
+
   if(argc != 2){
     printf("Invalid Arguments!\nUsage: %s <URL>\n", argv[0]);
     return -1;
   }
   struct linkInfo connectionInfo;
+  /*struct linkInfo{
+    char host[4048];
+    char path[4048];
+    char usr[4048];
+    char pw[4048];
+    char file[4048];
+    unsigned short port;
 
+  }; */
   parseLink(argv[1], &connectionInfo);
 
   char* address = transformHostName(connectionInfo.host);
+  printf("Init connection!\n");
+  sockfd = initConnection(address, connectionInfo.port);
+  printf("Connected!\n");
 
-  sockfd = initConnection(address, port);
+  char buff2[2000];
+  recv(sockfd,buff2,2000, 0);
+  printf("Buff2 Info: %s\n", buff2);
 
-  /*send a string to the server*/
-	bytes = write(sockfd, buf, strlen(buf));
-	printf("Bytes escritos %d\n", bytes);
-  /* */
-
-
+  ftp_login(sockfd, connectionInfo.usr, connectionInfo.pw);
+  ftp_changePath(sockfd, connectionInfo.path);
+  ftp_getFile(sockfd, connectionInfo.file);
+  ftp_disconnect(sockfd);
 	close(sockfd);
 	exit(0);
 }
