ab3415321a480b821d47d738dfa5b97be1b57772
David Joel Nogueira Azevedo <davidazevedo1996@hotmail.com>
2016-11-27 10:29:38 +0000

    Added individual scripts for each machine

---
+++ T2/tux62.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#Para executar e preciso dar permissao ao ficheiro "chmod +x tux62.sh"
+
+/etc/init.d/networking restart
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+echo 1 > /proc/sys/net/ipv4/ip_forward
+ifconfig eth0 up
+ifconfig eth0 172.16.61.1/24
+route add -net 172.16.60.0/24 gw 172.16.61.253
+route add default gw 172.16.61.254/24
+
+#Experiencia 4 ponto 4
+echo 0 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
+echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
---
+++ T2/tux64.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+#Para executar e preciso dar permissao ao ficheiro "chmod +x tux64.sh"
+
+/etc/init.d/networking restart
+echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+echo 1 > /proc/sys/net/ipv4/ip_forward
+ifconfig eth0 up
+ifconfig eth0 172.16.60.254/24
+route add default gw 172.16.61.254/24
+
+ifconfig eth1 up
+ifconfig eth1 172.16.61.253/24
+route add default gw 172.16.61.254/24 #nao tenho a certeza se e preciso repetir este comando
