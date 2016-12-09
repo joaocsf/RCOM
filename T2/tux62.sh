cd1ee5cc685e47bc99d32556505b6467161596e0
David Azevedo <davidazevedo1996@hotmail.com>
2016-12-09 12:52:35 +0000

    Update tux62.sh

--- T2/tux62.sh
+++ T2/tux62.sh
@@ -4,8 +4,7 @@
 
 updateimage
 /etc/init.d/networking restart
-echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-echo 1 > /proc/sys/net/ipv4/ip_forward
+
 ifconfig eth0 up
 ifconfig eth0 172.16.61.1/24
 route add -net 172.16.60.0/24 gw 172.16.61.253
