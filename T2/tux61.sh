90d26ea1945ec539baffd38a5c8f6126f728d0c3
David Azevedo <davidazevedo1996@hotmail.com>
2016-12-09 12:51:43 +0000

    Update tux61.sh

--- T2/tux61.sh
+++ T2/tux61.sh
@@ -3,8 +3,6 @@
 #Para executar e preciso dar permissao ao ficheiro "chmod +x tux61.sh"
 updateimage
 /etc/init.d/networking restart
-echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-echo 1 > /proc/sys/net/ipv4/ip_forward
 
 ifconfig eth0 up
 ifconfig eth0 172.16.60.1/24
